e356e27d6a05633970ce7c858c7102c1bc8eb132
wat1881mayer <happymayer2013@icloud.com>
2020-12-06 16:59:15 +0900

    setup react context provider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
 
 const Events = ({state,dispatch})=>{
+    const value = useContext(AppContext)
     return (
         <>
-        <h4>イベント一覧</h4>
-        <table className="table table-hover">
-        <thead>
-            <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            </tr>  
-        </thead>
-        <tbody>
-            {state.map((event,index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
-            
-        </tbody>
-        </table>
-    </>
+            <div>{value}</div>
+            <h4>イベント一覧</h4>
+            <table className="table table-hover">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>タイトル</th>
+                <th>ボディー</th>
+                </tr>  
+            </thead>
+            <tbody>
+                {state.map((event,index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+                
+            </tbody>
+            </table>
+            </>
     )
 }
 export default Events
